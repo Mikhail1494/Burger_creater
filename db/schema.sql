50bd2d243ea0635267fcbf6d497a20fc68d0b90b
Mikhail Mednikh <mmednikh@gmail.com>
2020-05-26 15:34:23 -0400

    cheanged mysql filr in burger app

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,18 @@
-CREATE DATABASE burgers_db;
+CREATE DATABASE
+IF NOT EXISTS burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE burgers_db (
+DROP TABLE IF EXISTS burgers;
 
-	id INT NOT NULL AUTO_INCREMENT,
-	burger_name VARCHAR(255) NOT NULL,
+CREATE TABLE burgers
+(
+
+	id INT NOT NULL
+	AUTO_INCREMENT,
+	burger_name VARCHAR
+	(255) NOT NULL,
     devoured BOOLEAN,
-	PRIMARY KEY (id)
+	PRIMARY KEY
+	(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,12 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Protein Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Hamburger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Cheeseburger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Protein Burger', false);
